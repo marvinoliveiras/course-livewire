6b7e903348d38d42f267a8cb6d3ec460b62043d5
marvinoliveiras <marvinoliveiras@yahoo.es>
2021-06-15 20:58:19 +0100

    refactoring uploadPhoto controller to delete old photo, because when old photo file extension is different then new photo, the old photo isnt deleted

--- app/Http/Livewire/User/UploadPhoto.php
+++ app/Http/Livewire/User/UploadPhoto.php
@@ -21,28 +21,43 @@ class UploadPhoto extends Component
         ]);
         $user = auth()->user();
         $path = $this->uploadPhoto(
-            $user->name
+            $user
         );
         if($path){
             $this->updateDatabase($user,
                 $path
             );
-            return redirect()
-                ->route('tweets.index');
+            //return redirect()                ->route('tweets.index');
         }
     }
-    protected function uploadPhoto($userName)
+    protected function uploadPhoto($user)
     {
-        $nameFile = Str::slug($userName)
+        $this->deleteOldPhoto($user
+            ->profile_photo_path
+        );
+        $nameFile = Str::slug($user->name)
             .'.'.$this->photo
             ->getClientOriginalExtension();
         return $this->photo
-            ->storeAs('users',$nameFile);
+            ->storeAs('users',
+                $nameFile
+            );
     }
     protected function updateDatabase(
         $user, $path)
     {
         $user->profile_photo_path = $path;
         $user->update();
+    }
+    protected function deleteOldPhoto($oldPhoto)
+    {
+        $completePathOldPhoto = __DIR__
+            .'/../../../../storage/app/public/'
+            .$oldPhoto;
+        if($oldPhoto != null && is_file(
+            $completePathOldPhoto)
+        ){
+            unlink($completePathOldPhoto);
+        }
     }
 }
